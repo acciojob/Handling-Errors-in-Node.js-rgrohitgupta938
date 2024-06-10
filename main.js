2e0115f5e712307e1cc258b5647dac2b13ecb09b
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-06-10 23:44:12 +0530

    sec

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 
-function printFileContents(filePath) {
-  fs.readFile(filePath, "utf-8", (err, data) => {
-    if (err) {
-      console.error("Column '${columnName}' not found in the CSV.");
-    } else {
-      console.log(data);
-    }
-  });
-}
-
+// Get the file path from the command-line arguments
 const filePath = process.argv[2];
 
-if (!filePath) {
-  console.error("Please provide a file path as a command-line argument.");
-  process.exit(1);
-}
-
-const absolutePath = path.resolve(filePath);
-
-printFileContents(absolutePath);
+// Read the file
+fs.readFile(path.join(__dirname, filePath), "utf8", function (err, data) {
+  if (err) {
+    // Print error message for invalid file path
+    console.log(`Error: ${err.message}`);
+  } else {
+    // Print the file content
+    console.log(data);
+  }
+});
